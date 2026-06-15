1c239c077be6d73739d180c6c7db1a9cbe21157a
FedericoBarberon <jfedericobarberonj@gmail.com>
2026-06-15 02:35:53 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,18 @@ fn main() {
             .expect("Failed to read stdin");
 
         let input = input.trim();
+        let mut tokens = input.split_whitespace();
 
-        if input == "exit" {
-            break;
+        let Some(command) = tokens.next() else {
+            continue;
+        };
+
+        let args = tokens.collect::<Vec<&str>>();
+
+        match command {
+            "exit" => break,
+            "echo" => println!("{}", args.join(" ")),
+            _ => println!("{input}: command not found"),
         }
-
-        println!("{input}: command not found");
     }
 }
